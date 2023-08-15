42d4e2f73659852b25861414d9d8664a60d9833f
mazarona <mazenfahem@gmail.com>
2023-08-15 10:51:00 +0300

    remove unwanted files

--- src/chip8_registers.d
+++
@@ -1,5 +0,0 @@
-src/chip8_registers.o: src/chip8_registers.c /usr/include/stdc-predef.h \
- include/chip8_registers.h include/chip8_config.h
-/usr/include/stdc-predef.h:
-include/chip8_registers.h:
-include/chip8_config.h:
